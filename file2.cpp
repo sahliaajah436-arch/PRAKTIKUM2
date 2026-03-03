f6fd74906a9715931224cc6aa1d16577f3e7b267
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-03-03 22:21:55 +0700

    implementasi prosedur dan fungsi

--- file2.cpp
+++ file2.cpp
@@ -1,3 +1,14 @@
 //library
 #include <iostream>
 using namespace std;
+
+//deklarasi global
+int p, l, luas;
+
+//implementasi prosedur dan fungsi
+void input(){
+    cout << "Masukkan panjang = ";
+    cin >> p;
+    cout << "Masukkan lebar = ";
+    cin >> l;
+}
