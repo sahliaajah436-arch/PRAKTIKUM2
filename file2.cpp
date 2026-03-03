a18bf6c08f557b6003123dc90b4b45e876b0b0f5
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-03-03 22:23:22 +0700

    menambahkan hasil penjumlahan

--- file2.cpp
+++ file2.cpp
@@ -21,3 +21,13 @@ int luasPersegi(int a, int b) {
     return a* b;
 }
 
+void output() {
+    cout << "Hasilnya = " << luasPersegi(p,l) << endl;
+    cout << "terima kasih";
+}
+
+int main() { //mulai
+    input();
+    output();
+    cout << "hasil penjumlahan " << jumlah(15.1, 24.9, 67);
+} //selesai
