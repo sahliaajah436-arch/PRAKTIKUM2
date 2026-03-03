f6f822aa64ac3efaad054fcab06ffae4a7767a90
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-03-03 22:26:42 +0700

    menambahkan jari jari lingkaran

--- file3.cpp
+++ file3.cpp
@@ -1,3 +1,22 @@
 #include <iostream>
 using namespace std;
 
+    //deklarasi variable
+    float jarijari;
+    float phi =3.14159;
+    //bagian fungsi
+    void awal(){
+        cout << "Masukkan jari-jari lingkaran = ";
+        cin >> jarijari;    
+    }
+
+    float luas (float jari_jari, float phi){
+        return phi * jarijari * jarijari;
+    }
+    void akhir (){
+        cout << "hasil luas untuk lingkaran dengan jari-jari " << jarijari<< luas(jarijari, phi);
+    }
+    int main (){
+        awal();
+        akhir();
+    }
