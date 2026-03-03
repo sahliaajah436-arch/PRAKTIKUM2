b6d0cc4e525b79cdfb57302c9d3cbcd3ea2f552e
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-03-03 22:14:31 +0700

    menambahkan namespace

--- file1.cpp
+++ file1.cpp
@@ -1,28 +1,3 @@
 //library
 #include <iostream>
 using namespace std;
-
-//deklarasi global
-int p, l, luas;
-
-//implementasi prosedur dan fungsi
-void input(){
-    cout << "Masukkan panjang = ";
-    cin >> p;
-    cout << "Masukkan lebar = ";
-    cin >> l;
-}
-
-int luasPersegi() {
-    return p * l;
-}
-
-void output() {
-    cout << "Hasilnya = " << luasPersegi() << endl;
-    cout << "terima kasih";
-}
-
-int main() { //mulai
-    input();
-    output();
-} //selesai
