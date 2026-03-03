0ba71316097173fba3444a182126d34f306535d1
sahliaajah436-arch <sahliaajah436@gmail.com>
2026-03-03 22:18:54 +0700

    memasukkan panjang, lebar, luas

--- file1.cpp
+++ file1.cpp
@@ -1,3 +1,19 @@
 //library
 #include <iostream>
 using namespace std;
+
+//deklarasi global 
+int p, l, luas;
+
+//implementasi prosedur dan fungsi
+void input(){
+    cout << "Masukkan panjang = ";
+    cin >> p;
+    cout << "Masukkan lebar = ";
+    cin >> l;
+}
+
+int luasPersegi() {
+    return p * l;
+}
+
